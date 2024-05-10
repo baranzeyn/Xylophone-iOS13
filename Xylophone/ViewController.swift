2dac82a0d96f641136e06762681706b3cff11449
Zeynep Baran <120269144+baranzeyn@users.noreply.github.com>
2024-05-10 11:30:01 +0300

    commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,32 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func keyPressed(_ sender: UIButton) { 
+        print("Start")
+        
+        sender.layer.opacity*=0.5
+        playSound(buttonString:sender.titleLabel?.text)
+        sender.layer.opacity*=2
+        sleep(2)
+        print("End")
+    }
     
-
+    func playSound(buttonString:String?) {
+        let url = Bundle.main.url(forResource: buttonString , withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player.play()
+                
+    }
 }
+
 
